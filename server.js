02759b5fc92fcbdad954b09b1f6b0299b9aab733
bmnazmussakib <nazmussakib161@gmail.com>
2023-03-21 17:49:24 +0600

    mongodb setup done

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const app = express();
-app.get('/', (req, res)=>{
-    res.send({
-        message: "Welcome To Uur Applicartion"
+app.use(morgan('dev'));
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false })) // Form Data Handle //
+app.use(bodyParser.json()) // Json Data Handle //
+
+
+
+app.get('/', async (req, res) => {
+    await res.send({
+        message: "Welcome To Our Application"
     })
 })
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, ()=>{
-    console.log(`SERVER IS RUNNING ON PORT: ${PORT} ` )
+app.listen(PORT, async () => {
+    console.log(`SERVER IS RUNNING ON PORT: ${PORT}`)
+    await mongoose.connect('mongodb://127.0.0.1:27017/money-app');
 })
